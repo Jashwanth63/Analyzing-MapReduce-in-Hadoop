0ec1e185136085290f449486a702f3784e836f0a
Jashwanth <jashwanth63@outlook.com>
2022-02-19 19:59:27 +0000

    Finished the workflow

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,15 @@
-from mrjob.job import MR
+from mrjob.job import MRJob as MR
 import re
 
 word_regex = re.compile(r"\b\w+\b") #RE to findall words in a string
 
 class analyze_MR(MR):
+    def mapper(self, _, line):
+        diff_words = word_regex.findall(line)
+        for words in diff_words:
+            yield(words.lower(), 1)
+    def reducer(self, words, counts):
+        yield(words, sum(counts))
 
 
 if __name__ == "__main__":
